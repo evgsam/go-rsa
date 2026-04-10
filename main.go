6365ae4bbd8cf990176fc1cb66e4331ffcb6908f
evgsam <evgsamarin@gmail.com>
2026-04-10 16:57:08 +0300

    Вероятностная проверка простоты по Миллеру–Рабину

--- main.go
+++ main.go
@@ -30,6 +30,69 @@ func randomNumberGen(bits int) (*big.Int, error) {
 	return n, nil
 }
 
+// Вероятностная проверка простоты по Миллеру–Рабину
+func isProbablyPrime(n *big.Int, rounds int) bool {
+	one := big.NewInt(1)
+	two := big.NewInt(2)
+	three := big.NewInt(3)
+	zero := big.NewInt(0)
+
+	// Простые базовые случаи
+	if n.Cmp(two) == 0 || n.Cmp(three) == 0 {
+		return true
+	}
+	if n.Cmp(two) < 0 || isEven(n) {
+		return false
+	}
+
+	// Представим n - 1 как 2^s * d, где d нечетное
+	nMinusOne := new(big.Int).Sub(n, one)
+	d := new(big.Int).Set(nMinusOne)
+	s := 0
+
+	for d.Bit(0) == 0 {
+		d.Rsh(d, 1)
+		s++
+	}
+
+	// rounds раундов теста
+	for i := 0; i < rounds; i++ {
+		// Случайное a в диапазоне [2, n-2]
+		max := new(big.Int).Sub(n, big.NewInt(3))
+		a, err := rand.Int(rand.Reader, max)
+		if err != nil {
+			return false
+		}
+		a.Add(a, two)
+
+		// x = a^d mod n
+		x := new(big.Int).Exp(a, d, n)
+
+		if x.Cmp(one) == 0 || x.Cmp(nMinusOne) == 0 {
+			continue
+		}
+
+		composite := true
+		for r := 1; r < s; r++ {
+			x.Exp(x, two, n)
+
+			if x.Cmp(nMinusOne) == 0 {
+				composite = false
+				break
+			}
+			if x.Cmp(zero) == 0 {
+				return false
+			}
+		}
+
+		if composite {
+			return false
+		}
+	}
+
+	return true
+}
+
 func main() {
 	n, _ := randomNumberGen(6)
 	fmt.Println(n)
